e5ff118037c210fc3390478c4063861fd4d68e30
bhavana-1-cmd <bhavnamishra.jbp@gmail.com>
2023-02-23 05:57:18 -0800

    second commit

---
+++ Bhavan-StackOverFlow-master/client/src/pages/Auth/AboutAuth.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AboutAuth = () => {
+  return (
+    <div>
+      <div className='auth-container-1'>
+        <h1>join the Stack Overflow community</h1>
+        <p>Get unstuck- ask a question </p>
+        <p> Unlock new privileges like voting and comment</p>
+        <p> Save your favorites filters,jobs </p>
+        <p> Earn reputation and badges </p>
+       <p styl={{fontSize:"13px", color:"#666767"}}>Collaborate and share knowledge with a private group for</p>
+       <p style={{ fontSize:"13px",color:"#007ac6"}}>Get Stack Overflow for teams free for up to 50 users. </p>
+      </div>
+    </div>
+  )
+}
+
+ export default AboutAuth
---
+++ Bhavan-StackOverFlow-master/client/src/pages/Questions/DisplayQuestion.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Leftsidebar from '../../components/LeftSidebar/Leftsidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import QuestionsDetails from './QuestionsDetails'
+
+const DisplayQuestion = () => {
+  return (
+          <div className='home-container-1'>
+           <Leftsidebar/>
+      <div className='home-container-2'>
+           <QuestionsDetails/>
+        <RightSidebar/>
+    </div>
+    </div>
+  )
+}
+
+export default DisplayQuestion
