530a26f1159d508af480f8c17a845cc33b37fa45
Shreyash Singh <65494781+shreyash001@users.noreply.github.com>
2023-03-27 14:12:01 +0530

    issue fixed

--- Bhavan-StackOverFlow-master/client/src/reducers/auth.js
+++ Bhavan-StackOverFlow-master/client/src/reducers/auth.js
@@ -1,9 +1,9 @@
 const authReducer =(state = {data: null}, actions) => {
  switch(actions.type){
-     case 'Auth':
-         return state
-        default:
-             return;
+    case 'Auth':
+        return state
+    default:
+        return state;
  }
 }
 export default authReducer
